7774c71f1a39b2c5a6ab736f26bb233eedf77fa2
Qinhua Yan <yan@adacore.com>
2014-12-04 18:46:23 -0500

    modified gitignore, add basic data processing from aubioresult to notes in string class in process.py

---
+++ src/process.py
@@ -0,0 +1,31 @@
+# normalize parameters
+INTERVAL_MAX = 2.0
+INTERVAL_PACE = 4
+
+# Notes processing class
+class Notes:
+  def __init__(self, f):
+    self.file = f
+
+  def generateNotes(f, upperbound=INTERVAL_MAX):
+    pitches = []
+    intervals = []
+    for line in open(f):
+      a = line.strip("\n").split()
+      interval = float(a[2])-float(a[1])
+      if interval < upperbound :
+        pitches.append(a[0])
+        intervals.append(interval)
+
+    return self.normalizeIntervals(pitches, intervals)
+
+  def normalizeIntervals(self, pitches, intervals):
+    # @ list of intervals, float
+    # @convert into normalize 1-4 string list
+
+    d = (max(intervals) - min(intervals))/INTERVAL_PACE
+    # !!!!!!!!!1
+
+    intervals = list("%d"round(e/d) for e in intervals)
+    norm_res = list(pitches[i] + intervals[i] for i in xrange(len(intervals)))
+    return norm_res
